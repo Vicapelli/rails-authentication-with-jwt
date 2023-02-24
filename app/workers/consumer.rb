7fa0d27da0c6b9ce581b2a96d8d66495733eec9d
Victor Capelli <victorbustocapelli@hotmail.com>
2023-02-23 21:45:44 -0300

    remove unused code

--- app/workers/consumer.rb
+++ app/workers/consumer.rb
@@ -1,13 +1,5 @@
 require 'sneakers'
 
-# conn = Bunny.new(hostname: "localhost:5672", username: "guest", password: "guest")
-# conn.start
-# ch = conn.create_channel
-# x = ch.direct("exchange.somavitu")
-# q = ch.queue("somavitu", durable: true)
-# q.bind(x)
-# ch.close
-
 class Consumer
   include Sneakers::Worker
   from_queue :somavitu
